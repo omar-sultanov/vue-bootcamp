cd4b8d996e415164f8deaf687e69a8fc917e7959
omar.sultanov.98 <omar.sultanov.98@gmail.com>
2024-02-07 21:54:37 +0300

    up

---
+++ lesson2/2-todo-app/src/components/ListSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <TodoList/>
+        <ResultSection />
+    </div>
+</template>
+<script>
+import ResultSection from './ResultSection.vue';
+import TodoList from './TodoList.vue';
+
+export default{
+    components:{TodoList, ResultSection},
+    // props:['todoList']
+}
+</script>
